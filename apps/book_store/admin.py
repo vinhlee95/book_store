809e70dc36c60442f42dea8c622c9f03a4b30da8
Vinh Le <lethanhvinh95@gmail.com>
2021-04-21 07:28:49 +0300

    register Book and Author admin

--- apps/book_store/admin.py
+++ apps/book_store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from apps.book_store.models import Author, Book
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
