050e6dfc0f32f7471e98ae9b5ef1c3a0fe29e6df
Vinh Le <lethanhvinh95@gmail.com>
2021-04-21 07:38:02 +0300

    remove __pycache__ from git

--- apps/book_store/admin.py
+++ apps/book_store/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from apps.book_store.models import Author, Book
 
+
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
+
 class BookAdmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
