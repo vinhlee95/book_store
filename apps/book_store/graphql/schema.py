f25e2a7aeac251149b6c090658442294ea47d2b2
Vinh Le <lethanhvinh95@gmail.com>
2021-04-22 08:05:05 +0300

    add interface and union

--- apps/book_store/graphql/schema.py
+++ apps/book_store/graphql/schema.py
@@ -16,10 +16,30 @@ class BookType(DjangoObjectType):
         fields = ("id", "title", "synopsis", "author", "published_date")
 
 
+# Interfaces
+class Animal(graphene.Interface):
+    name = graphene.String(required=True)
+    age = graphene.Int()
+
+
+class CatType(graphene.ObjectType):
+    class Meta:
+        interfaces = (Animal,)
+
+    breed = graphene.String(required=True)
+
+
+# Unions
+class SearchResultType(graphene.Union):
+    class Meta:
+        types = (AuthorType, BookType)
+
+
 class Query(graphene.ObjectType):
     authors = graphene.List(AuthorType)
     books = graphene.List(BookType)
     get_book_by_title = graphene.Field(type=BookType, title=graphene.String(required=True))
+    get_cat = graphene.Field(type=CatType)
 
     def resolve_authors(self, info):
         return Author.objects.all()
@@ -32,3 +52,7 @@ class Query(graphene.ObjectType):
             return Book.objects.get(title=title)
         except Book.DoesNotExist:
             return None
+
+    def resolve_get_cat(self, info):
+        return {"name": "foo", "age": 2, "breed": "bar"}
+
