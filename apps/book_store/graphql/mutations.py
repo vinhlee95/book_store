849f9de34a303011fb093ce73c184a3ad1e67b72
Vinh Le <lethanhvinh95@gmail.com>
2021-04-23 07:22:57 +0300

    add BookInput for specify input for adding new book

--- apps/book_store/graphql/mutations.py
+++ apps/book_store/graphql/mutations.py
@@ -4,19 +4,26 @@ from apps.book_store.models import Book, Author
 from apps.book_store.graphql.types import BookType
 
 
+class BookInput(graphene.InputObjectType):
+    title = graphene.String(required=True)
+    synopsis = graphene.String(required=True)
+    author_name = graphene.String(required=True)
+
+
 class CreateBook(graphene.Mutation):
     class Arguments:
-        title = graphene.String()
-        synopsis = graphene.String()
-        author_name = graphene.String()
+        book_data = BookInput(required=True)
 
     # Fields of the Mutation when it is resolved
     ok = graphene.Boolean()
     book = graphene.Field(lambda: BookType)
 
-    def mutate(self, info, title, synopsis, author_name):
-        author = Author.objects.get(name=author_name)
-        new_book = Book(title=title, synopsis=synopsis, author=author)
+    def mutate(self, info, book_data: BookInput = None):
+        if not book_data:
+            raise Exception("No book data provided.")
+
+        author = Author.objects.get(name=book_data.author_name)
+        new_book = Book(title=book_data.title, synopsis=book_data.synopsis, author=author)
         new_book.save()
         return CreateBook(ok=True, book=new_book)
 
